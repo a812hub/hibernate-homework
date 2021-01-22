a030ebdd2a17ab0160de88185ecee85c81f880d5
a812hub <yusupova.alla@gmail.com>
2021-01-22 15:39:06 +0300

    Configure entities

--- src/main/java/ru/hh/school/entity/Employer.java
+++ src/main/java/ru/hh/school/entity/Employer.java
@@ -1,24 +1,34 @@
 package ru.hh.school.entity;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-//TODO: оформите entity
+@Entity
+@Table(name = "employer")
 public class Employer {
 
+  @Id()
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "employer_id")
   private Integer id;
 
+  @Column(name = "company_name", nullable = false, length = 100)
   private String companyName;
 
-  // не используйте java.util.Date
-  // https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#basic-datetime-java8
+  @Column(name = "creation_time")
   private LocalDateTime creationTime;
 
+  @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "employer")
   private List<Vacancy> vacancies = new ArrayList<>();
 
+  @Column(name = "block_time")
   private LocalDateTime blockTime;
+
+  public Employer() {
+  }
 
   public List<Vacancy> getVacancies() {
     return vacancies;
@@ -44,10 +54,6 @@ public class Employer {
     this.blockTime = blockTime;
   }
 
-  // статьи на тему реализации equals() и hashCode():
-  //
-  // https://vladmihalcea.com/hibernate-facts-equals-and-hashcode/
-  // https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
--- src/main/java/ru/hh/school/entity/Resume.java
+++ src/main/java/ru/hh/school/entity/Resume.java
@@ -1,21 +1,14 @@
 package ru.hh.school.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-//TODO: оформите entity
+@Entity
+@Table(name = "resume")
 public class Resume {
-  // TODO: сделать так, чтобы id брался из sequence-а
-  // таким образом, мы сможем отправлять в бд запросы батчами.
-  // нужно учитывать, что описание sequence при создании таблицы также должно соответствовать
-  // хиберовской сущности (см. create_resume.sql)
-  //
-  // Подробнее:
-  // https://vladmihalcea.com/how-to-batch-insert-and-update-statements-with-hibernate/
-  // https://vladmihalcea.com/from-jpa-to-hibernates-legacy-and-enhanced-identifier-generators/
 
   @Id
-  @GeneratedValue(/* здесь место для вашего кода */)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence")
+  @SequenceGenerator(name = "sequence", sequenceName = "resume_id_seq", allocationSize = 10)
   private Integer id;
 
   private String description;
